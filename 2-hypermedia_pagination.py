3d4bd0da1caca5b6d675e7eed94336e969583a36
espoupou <126259211+espoupou@users.noreply.github.com>
2024-04-02 02:33:25 +0000

    Update 2-hypermedia_pagination.py

--- 2-hypermedia_pagination.py
+++ 2-hypermedia_pagination.py
@@ -1,1 +1,46 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Pagination"""
+import csv
+from math import ceil
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """return a tuple of size two containing a start index and an end index"""
+    start = (page - 1) * page_size
+    return (start, start + page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Hypermedia pagination"""
+        page_data = self.get_page(page, page_size)
+        total_pages = ceil(len(self.dataset()) / page_size)
+        next_page = page + 1 if page + 1 < total_pages else None
+        prev_page = page - 1 if page - 1 > 1 else None
+
+        return {
+            "page_size": len(page_data),
+            "page": page,
+            "data": page_data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
