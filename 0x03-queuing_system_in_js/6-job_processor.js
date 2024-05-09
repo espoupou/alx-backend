0b430d56dcbb778cfd4174027d840271e31df7a9
espoupou <espoirativon@gmail.com>
2024-05-09 20:42:22 +0000

    all

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+import kue from "kue";
+
+const queue = kue.createQueue();
+
+function sendNotification(phoneNumber, message) {
+  console.log(
+    `Sending notification to ${phoneNumber}, with message: ${message}`
+  );
+}
+
+const queueKey = "push_notification_code";
+
+queue.process(queueKey, (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message);
+  done();
+});
